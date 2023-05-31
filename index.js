0709bdb5f7797f290ef1fd60578609fea89c572c
JacobFla <j.flaskamp@web.de>
2023-05-31 13:20:54 +0200

    base app

---
+++ index.js
@@ -0,0 +1,10 @@
+//promises ???
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("service-worker.js").then(registration => {
+        console.log("SW registered!");
+        console.log(registration);
+    }).catch(error => {
+        console.log("SW registration failed!");
+        console.log(error);
+    });
+}
